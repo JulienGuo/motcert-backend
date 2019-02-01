34ec662ff4943e02728c4e512568718be8f8f4ee
郭 立冬 <guolidong@chainnova.com>
2019-02-01 11:27:29 +0800

    漏掉了fabric-sdk-go
    Signed-off-by: 郭 立冬 <guolidong@chainnova.com>

---
+++ app/vendor/github.com/hyperledger/fabric-sdk-go/test/fixtures/fabric/v1/crypto-config/env.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+export ORG1CA1_FABRIC_CA_SERVER_CA_KEYFILE=/etc/hyperledger/fabric-ca-server-config/77d010a6b522adcb065cffeab213f18e1c8348124e09df6cf089c145ab4fcaca_sk
+export ORG2CA1_FABRIC_CA_SERVER_CA_KEYFILE=/etc/hyperledger/fabric-ca-server-config/b34357943d841ca0eae8bbee957b8333279edc3c33de1547ea14ec14eb7ed7b4_sk
